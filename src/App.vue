07e8c5e4051cd787dbcf5f209bb9d877145a9728
Ethan Barrett <hello@buildingsareheavy.com>
2019-02-22 15:11:38 -0700

    accidentally added the old get request when adjusting the readme. Also I need to refactor everything because it's all in the wrong place. UGH

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
   <div id="app">
-    <API>BOO</API>
+    <h1>PLANTS!</h1>
+    <div v-for="(data) in info.data">
+      <div class="data data-all">
+        <div class="data">
+          <span class="name-science">Scientific Name:</span>
+          {{ data.Genus }} {{ data.Species }}
+        </div>
+        <div class="data" v-if="data.Common_Name">
+          <span class="name-common">Common Name:</span>
+          {{ data.Common_Name }}
+        </div>
+        <div class="data" v-else>
+          <span class="name-common-none">Common name doesn't exist</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import API from "./components/Api.vue";
 export default {
-  name: "app",
-  components: {
-    API
+  name: "API",
+  data() {
+    return {
+      info: null,
+      loading: true,
+      errored: false
+    };
+  },
+  mounted() {
+    this.axios
+      .get("https://plantsdb.xyz/search?limit=10")
+      .then(response => (this.info = response.data))
+      .catch(error => {
+        // console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="scss">
+.data-all {
+  margin: 2rem auto;
+  display: inline-block;
+  background: black;
+}
+
+.data {
+  padding: 0.5rem;
+  color: white;
+  span {
+    padding: 0.25rem 0.5rem;
+    color: black;
+  }
+}
+.name-science {
+  background: skyblue;
+}
+.name-common {
+  background: lightgreen;
+}
+.name-common-none {
+  background: tomato;
 }
 </style>
